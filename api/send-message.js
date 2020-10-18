6b92cc2138dbe62f1b3364e1f2c4ef15619c6e8f
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-18 00:35:23 -0500

    Update send-message.js

--- api/send-message.js
+++ api/send-message.js
@@ -2,10 +2,15 @@ var fetch = require('node-fetch');
 var btoa = require('btoa');
 
 module.exports = (req, res) => {
-    fetch('https://api.twilio.com/2010-04-01/Accounts/' + req.params.aid + '/Messages.json', {
+    fetch('https://api.twilio.com/2010-04-01/Accounts/' + process.env.ACCOUNT_ID + '/Messages.json', {
         method: 'POST',
         headers: {
-            'Authorization': 'Basic ' + btoa(req.params.aid)
+            'Authorization': 'Basic ' + btoa(`${process.env.ACCOUNT_ID}:${process.env.AUTH_KEY}`)
+        }
+        body: {
+            'Body': req.params.body,
+            'From': process.env.PHONE_NUMBER,
+            'To': req.params.to
         }
     });
 }
