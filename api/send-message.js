4af00516ccab7417acc9661483c55b7b10d82cb9
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-18 10:49:02 -0500

    Update send-message.js

--- api/send-message.js
+++ api/send-message.js
@@ -5,13 +5,13 @@ module.exports = (req, res) => {
   try {
     fetch('https://api.twilio.com/2010-04-01/Accounts/' + process.env.ACCOUNT_ID + '/Messages.json', {
       method: 'POST',
-      headers: {
-        'Authorization': 'Basic ' + btoa(`${process.env.ACCOUNT_ID}:${process.env.AUTH_KEY}`)
-      },
       body: {
         'Body': req.params.body,
         'From': process.env.PHONE_NUMBER,
         'To': req.params.to
+      },
+      headers: {
+        'Authorization': 'Basic ' + btoa(`${process.env.ACCOUNT_ID}:${process.env.AUTH_KEY}`)
       }
     }).then(data => res.send(data.data)).catch(err => res.send(err));
   } catch (err) {
