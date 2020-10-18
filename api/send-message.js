f214316e378a01e3c5453c132149e8f9a81f0212
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-18 11:18:12 -0500

    req.params -> req.query

--- api/send-message.js
+++ api/send-message.js
@@ -2,24 +2,23 @@ var fetch = require('node-fetch');
 var btoa = require('btoa');
 
 module.exports = (req, res) => {
-  if (req.params.body && req.params.to) {
-  try {
-    console.log(req.params);
-    fetch('https://api.twilio.com/2010-04-01/Accounts/' + process.env.ACCOUNT_ID + '/Messages.json', {
-      method: 'POST',
-      body: {
-        'Body': req.params.body,
-        'From': process.env.PHONE_NUMBER,
-        'To': req.params.to
-      },
-      headers: {
-        'Authorization': 'Basic ' + btoa(`${process.env.ACCOUNT_ID}:${process.env.AUTH_KEY}`)
-      }
-    }).then(data => res.send(data.data)).catch(err => res.send(err));
-  } catch (err) {
-    res.send(err);
-  }
-  } else {
-    res.send("Yeehaw! You're missing a parameter!");
-  }
+    if (req.query.body && req.query.to) {
+        try {
+            fetch('https://api.twilio.com/2010-04-01/Accounts/' + process.env.ACCOUNT_ID + '/Messages.json', {
+                method: 'POST',
+                body: {
+                    'Body': req.query.body,
+                    'From': process.env.PHONE_NUMBER,
+                    'To': req.query.to
+                },
+                headers: {
+                    'Authorization': 'Basic ' + btoa(`${process.env.ACCOUNT_ID}:${process.env.AUTH_KEY}`)
+                }
+            }).then(data => res.send(data.data)).catch(err => res.send(err));
+        } catch (err) {
+            res.send(err);
+        }
+    } else {
+        res.send("Yeehaw! You're missing a parameter!");
+    }
 }
