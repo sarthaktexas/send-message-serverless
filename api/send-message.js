daddf7ec20eec005e34d37cf08c516904fcebc1b
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-18 11:07:17 -0500

    Update send-message.js

--- api/send-message.js
+++ api/send-message.js
@@ -2,7 +2,9 @@ var fetch = require('node-fetch');
 var btoa = require('btoa');
 
 module.exports = (req, res) => {
+  if (req.params.body && req.params.to) {
   try {
+    console.log(req.params);
     fetch('https://api.twilio.com/2010-04-01/Accounts/' + process.env.ACCOUNT_ID + '/Messages.json', {
       method: 'POST',
       body: {
@@ -16,5 +18,8 @@ module.exports = (req, res) => {
     }).then(data => res.send(data.data)).catch(err => res.send(err));
   } catch (err) {
     res.send(err);
+  }
+  } else {
+    res.send("Yeehaw! You're missing a parameter!");
   }
 }
